03e4e81e349e11cbef85eeb3c584f14dcc01904d
Ahmet CAN <cnahmet@outlook.com>
2021-10-25 11:47:09 +0300

    Update map.blade.php

--- resources/views/expense/map.blade.php
+++ resources/views/expense/map.blade.php
@@ -5,7 +5,7 @@
 @section('extras')
 <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
-<script type="text/javascript" src="https://maps.googleapis.com/maps/api/js?key=AIzaSyBUcDuhA3sF0SsvlI9MTbFzYR98mH3xyMM&libraries=places">
+<script type="text/javascript" src="https://maps.googleapis.com/maps/api/js?key=KEY&libraries=places">
 </script>
 
 <style type="text/css">
